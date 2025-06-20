5c9dc6b5a989b4d496b200740034e80b3e649f4c
Guillem Senabre i Prades <guillemsenabre@gmail.com>
2025-06-20 16:05:09 +0200

    Allocate aligned heap memory using posix_memalign() method

--- bus_latency/lat_disk.c
+++ bus_latency/lat_disk.c
@@ -3,11 +3,23 @@
 #include <fcntl.h>
 
 #define SIZE (4*1024) // 4 KiB
+#define ALIGNMENT 512 // 512 B
+
+int main() {	
+	void *aligned_memory;
+	
+	size_t size = SIZE;
+	size_t alignment = ALIGNMENT;
 
 
-int main() {
 	int fd = open("./files/disk_lat", O_WRONLY | O_CREAT | O_SYNC | O_DIRECT, 0666 );
 	
 	if (fd < 0) { perror("open"); return 1; }
+	
+
+	if (posix_memalign(void &aligned_memory, alignment, size) != 0) {
+		perror("memalign");
+		return 1;
+	}
 }
 
