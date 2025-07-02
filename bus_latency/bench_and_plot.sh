57a73b34bfd43a21e923773ae4bf35f763c0f3a4
Guillem Senabre i Prades <guillemsenabre@gmail.com>
2025-07-02 11:02:32 +0200

    Refactore bash script to pass args to python script

--- bus_latency/bench_and_plot.sh
+++ bus_latency/bench_and_plot.sh
@@ -6,6 +6,11 @@ TARGET_MACHINE_DIRECTORY="/home/${TARGET_MACHINE_USER}//projects/vitamin-v-wp4-b
 
 LOCAL_PLOT_DIRECTORY="./plots/"
 
+ITERATIONS=100
+NUMBER_VALUES=$ITERATIONS
+MACHINE_NAME="x86"
+SIZE=512
+
 # Check if lat_disk binary exists and is executable
 if [[ ! -x ./lat_disk ]]; then
     echo "lat_disk binary not found or not executable. Compiling..."
@@ -17,14 +22,14 @@ if [[ ! -x ./lat_disk ]]; then
 fi
 
 # Run the binary
-./lat_disk
+./lat_disk -i $ITERATIONS
 if [[ $? -ne 0 ]]; then
     echo "lat_disk execution failed. Exiting."
     exit 1
 fi
 
 # Run the plotting script
-python3 lat_plot.py
+python3 lat_plot.py $MACHINE_NAME $NUMBER_VALUES $SIZE
 if [[ $? -ne 0 ]]; then
     echo "Python plotting script failed."
     exit 1
